209deb8c65814e1dab4e5bedca1b3af63540c9ac
Sam Belliveau <sam.belliveau@gmail.com>
2021-10-07 19:37:51 -0400

    update string library

--- mystring.c
+++ mystring.c
@@ -3,49 +3,43 @@
 int mystrlen(const char* s)
 {
 	const char* e = s;
-	while (*++e);
+	while (*e) e++;
 	return e - s;
 }
 
 char* mystrcpy(char *dest, const char *src)
 {
-	char* d = dest;
-	while (*src) 
-		*(dest++) = *(src++);
-	return d;  
+	char* t = dest;
+	while (*(dest++) = *(src++));
+	return t;  
 }
 
 char* mystrncpy(char *dest, const char *src, unsigned n)
 {
-	const char* m = src + n;
-	char* d = dest;
-	
-	while (*src && src < m) 
-		*(dest++) = *(src++);
-	
-	return d;
+	char* t = dest;
+	while ((*(dest++) = *(src++)) && n--);
+	return t;
 }
 
 char* mystrcat(char *dest, const char *src)
 {
-	char *d = dest;
-	
-	while (*(dest++));
-	while (*src) 
-		*(dest++) = *(src++);
-	
-	return d;
+	char* t = dest;
+
+	while (*dest) dest++;
+	while (*(dest++) = *(src++));
+
+	return t;
 }
 
 char* mystrncat(char *dest, const char *src, unsigned n)
 {
-	const char *m = src + n;
-	char *d = dest;
-	
-	while (*(dest++));
-	while (*src && src < m) *(dest++) = *(src++);
-	
-	return d;
+	char* t = dest;
+
+	while (*dest) dest++;
+	while ((*(dest++) = *(src++)) && --n);
+	if (!n) *dest = '\0';
+
+	return t;
 }
 
 int mystrcmp(const char *a, const char *b)
@@ -55,7 +49,7 @@ int mystrcmp(const char *a, const char *b)
 	do {
 		r = (int)*a - (int)*b;
 		if (r) return r;
-	} while (*(++a) | *(++b));
+	} while (*++a | *++b);
 	
 	return 0;
 }
@@ -64,7 +58,7 @@ char* mystrchr(char *s, char c)
 {
 	do { 
 		if (*s == c) return s;
-	} while(*++s);
+	} while (*(s++));
 	
 	return 0;
 }
